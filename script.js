f89b519a20751ad03c92fc5be7bc7f91958486fa
HackingDon <udhaya34.raj34@gmail.com>
2024-07-16 19:53:01 +0530

    stylesheet

---
+++ script.js
@@ -0,0 +1,33 @@
+let input = document.getElementById('input');
+let buttons = document.getElementsByClassName('buttons');
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click',(e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'D'){
+            string = string.substring(0,string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
+});
+const toggle = document.querySelector('.toggler-icon');
+const calculator = document.querySelector('.calculator')
+toggle.addEventListener('click',() =>{
+    
+    calculator.classList.toggle('dark');
+    calculator.classList.toggle('light');
+})
